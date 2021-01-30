c453b0b1713c2ebe479f1f96b74cfcb0d9b32557
joaoffnascimento <thecurrentuser@live.com>
2021-01-30 13:55:06 -0300

    chore(PilotosController): add get piloto mock endpoints

--- piloto/Controllers/PilotosController.cs
+++ piloto/Controllers/PilotosController.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using piloto.Data;
+using piloto.Models;
 
 namespace piloto.Controllers
 {
@@ -6,6 +9,23 @@ namespace piloto.Controllers
     [ApiController]
     public class PilotosController : ControllerBase
     {
-        
+
+        private readonly MockPilotoRepository _repository = new MockPilotoRepository();
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Piloto>> GetAllPilotos()
+        {
+            var pilotos = _repository.getAllPilotos();
+
+            return Ok(pilotos);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Piloto> GetPiloto(int id)
+        {
+            var piloto = _repository.getPilotoById(id);
+
+            return Ok(piloto);
+        }
     }
 }
