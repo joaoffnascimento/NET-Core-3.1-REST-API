1c02735e2da16b7df76831461d1a6806bff96773
joaoffnascimento <thecurrentuser@live.com>
2021-02-03 23:50:30 -0300

    chore(Piloto): add db decorators annotations

--- piloto/Models/Piloto.cs
+++ piloto/Models/Piloto.cs
@@ -1,10 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace piloto.Models
 {
     public class Piloto
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Equipe { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Nome { get; set; }
+
+        [Required]
         public int Idade { get; set; }
     }
 }
