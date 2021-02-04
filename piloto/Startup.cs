df0e1e8bc49b35b04613b2c7bf9f65b631685a55
joaoffnascimento <thecurrentuser@live.com>
2021-02-03 22:48:08 -0300

    chore(Startup): register IPilotoRepository

--- piloto/Startup.cs
+++ piloto/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using piloto.Data;
 
 namespace piloto
 {
@@ -26,12 +27,14 @@ namespace piloto
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "piloto", Version = "v1" });
             });
+
+            services.AddScoped<IPilotoRepository, MockPilotoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
